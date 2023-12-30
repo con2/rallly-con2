f95a5c5204944089430bb088ffcb834f677b2ced
Santtu Pajukanta <santtu@pajukanta.fi>
2023-12-30 09:27:15 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Con2 Kubernetes deployment of Rallly
+
+[Rallly](https://github.com/lukevella/rallly) is a tool to, according to them,
+
+> Schedule group meetings with friends, colleagues and teams. Create meeting polls to find the best date and time to organize an event based on your participants' availability. Save time and avoid back-and-forth emails.
+
+This repository provides a Kubernetes deployment of Rallly that is setup to authenticate against [Kompassi](https://github.com/con2/kompassi) using OIDC.
+
+User friendly redirects to `rallly.con2.fi` (such as `rally.con2.fi`) are managed under [redirects](https://github.com/con2/redirects). (Note that in every other instance of `rallly` there are three lower-case L letters.)
+
+## Getting started
+
+Requirements:
+
+* A Kubernetes cluster
+* [Skaffold](https://skaffold.dev)
+* Node.js (tested: 20, 21)
+
+For a macOS workstation using Homebrew, install local dependencies using
+
+    brew install skaffold kubernetes-cli node
+    npm install
+
+Generate Kubernetes manifests using
+
+    npm run k8s:generate
+
+This runs the `kubernetes/manifest.ts` that  is intended to be a brutally simple Node.js program with no other dependencies than `fs`. It accepts one environment variable, `ENV`, with the values `dev` (the default) and `production`, and outputs Kubernetes manifests as JSON files in the directory it was run.
+
+Test with Skaffold (with Docker Desktop or similar local Kubernetes cluster):
+
+    npm run k8s:dev
+
+Assuming you have an [ingress controller set up](https://outline.con2.fi/doc/ingress-controller-XfVUOHtp2t#h-installing-an-ingress-controller-for-local-development), you should now be able to view the UI at http://rallly.localhost.
+
+For staging and production, deployment is done in two steps using Skaffold:
+
+    cd kubernetes && ENV=staging npx ts-node manifest.ts && cd -
+    skaffold run -n rallly
+
+See `skaffold.yaml` in the repository root.
+
+You should, for the most part, not deploy manually. GitHub Actions CI/CD is set up to deploy all commits to `main` into the staging environment at https://v2.dev.kompassi.eu, and after a manual gate into production at https://v2.kompassi.eu. See `.github/workflows/cicd.yaml`.
---
+++ kubernetes/manifest.ts
@@ -0,0 +1,340 @@
+import { writeFileSync, unlinkSync, existsSync } from "fs";
+
+interface Environment {
+  hostname: string;
+  secretManaged: boolean;
+  postgresManaged: boolean;
+  kompassiBaseUrl: string;
+  tlsEnabled: boolean;
+}
+
+type EnvironmentName = "dev" | "production";
+const environmentNames: EnvironmentName[] = ["dev", "production"];
+
+const environmentConfigurations: Record<EnvironmentName, Environment> = {
+  dev: {
+    hostname: "rallly.localhost",
+    secretManaged: true,
+    postgresManaged: true,
+    kompassiBaseUrl: "https://dev.kompassi.eu",
+    tlsEnabled: false,
+  },
+  production: {
+    hostname: "rallly.con2.fi",
+    secretManaged: false,
+    postgresManaged: false,
+    kompassiBaseUrl: "https://kompassi.eu",
+    tlsEnabled: true,
+  },
+};
+
+function getEnvironmentName(): EnvironmentName {
+  const environmentName = process.env.ENV;
+  if (!environmentNames.includes(environmentName as EnvironmentName)) {
+    return "dev";
+  }
+  return environmentName as EnvironmentName;
+}
+
+const environmentConfiguration =
+  environmentConfigurations[getEnvironmentName()];
+
+export const stack = "rallly";
+const image = "lukevella/rallly:latest";
+const nodeServiceName = "rallly";
+const clusterIssuer = "letsencrypt-prod";
+const tlsSecretName = "ingress-letsencrypt";
+const port = 3000;
+
+// only used if postgresManaged is true (dev)
+const insecurePostgresPassword = "postgres";
+
+const {
+  hostname,
+  secretManaged,
+  postgresManaged,
+  kompassiBaseUrl,
+  tlsEnabled,
+} = environmentConfiguration;
+
+const ingressProtocol = tlsEnabled ? "https" : "http";
+const publicUrl = `${ingressProtocol}://${hostname}`;
+
+// Startup and liveness probe
+const probe = {
+  httpGet: {
+    // TODO
+    path: "/",
+    port,
+    httpHeaders: [
+      {
+        name: "host",
+        value: hostname,
+      },
+    ],
+  },
+};
+
+export function labels(component?: string) {
+  return {
+    stack,
+    component,
+  };
+}
+
+function secretKeyRef(key: string) {
+  return {
+    secretKeyRef: {
+      name: stack,
+      key,
+    },
+  };
+}
+
+const env = Object.entries({
+  PORT: port,
+  DATABASE_URL: secretKeyRef("DATABASE_URL"),
+  SECRET_PASSWORD: secretKeyRef("SECRET_PASSWORD"),
+  NEXT_PUBLIC_BASE_URL: publicUrl,
+  SUPPORT_EMAIL: "tekniikka@tracon.fi",
+  SMTP_HOST: "mailer.b2.fi",
+  SMTP_TLS_ENABLED: true,
+  OIDC_NAME: "Kompassi",
+  OIDC_DISCOVERY_URL: `${kompassiBaseUrl}/oidc/.well-known/openid-configuration`,
+  OIDC_CLIENT_ID: secretKeyRef("OIDC_CLIENT_ID"),
+  OIDC_CLIENT_SECRET: secretKeyRef("OIDC_CLIENT_SECRET"),
+}).map(([key, value]) => {
+  if (value instanceof Object) {
+    return {
+      name: key,
+      valueFrom: value,
+    };
+  } else {
+    return {
+      name: key,
+      value: `${value}`,
+    };
+  }
+});
+
+// Node (Next.js) service
+const deployment = {
+  apiVersion: "apps/v1",
+  kind: "Deployment",
+  metadata: {
+    name: nodeServiceName,
+    labels: labels(nodeServiceName),
+  },
+  spec: {
+    selector: {
+      matchLabels: labels(nodeServiceName),
+    },
+    template: {
+      metadata: {
+        labels: labels(nodeServiceName),
+      },
+      spec: {
+        enableServiceLinks: false,
+        securityContext: {
+          runAsUser: 1000,
+          runAsGroup: 1000,
+          fsGroup: 1000,
+        },
+        initContainers: [],
+        containers: [
+          {
+            name: nodeServiceName,
+            image,
+            env,
+            ports: [{ containerPort: port }],
+            securityContext: {
+              readOnlyRootFilesystem: false,
+              allowPrivilegeEscalation: false,
+            },
+            startupProbe: probe,
+            livenessProbe: probe,
+          },
+        ],
+      },
+    },
+  },
+};
+
+const service = {
+  apiVersion: "v1",
+  kind: "Service",
+  metadata: {
+    name: nodeServiceName,
+    labels: labels(nodeServiceName),
+  },
+  spec: {
+    ports: [
+      {
+        port,
+        targetPort: port,
+      },
+    ],
+    selector: labels(nodeServiceName),
+  },
+};
+
+// PostgreSQL (only deployed if postgresManaged is true)
+const postgresDeployment = {
+  apiVersion: "apps/v1",
+  kind: "Deployment",
+  metadata: {
+    name: "postgres",
+    labels: labels("postgres"),
+  },
+  spec: {
+    replicas: 1,
+    selector: {
+      matchLabels: labels("postgres"),
+    },
+    template: {
+      metadata: {
+        labels: labels("postgres"),
+      },
+      spec: {
+        containers: [
+          {
+            name: "postgres",
+            image: "postgres:15",
+            ports: [
+              {
+                containerPort: 5432,
+              },
+            ],
+            env: [
+              {
+                name: "POSTGRES_PASSWORD",
+                value: insecurePostgresPassword,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
+};
+
+const postgresService = {
+  apiVersion: "v1",
+  kind: "Service",
+  metadata: {
+    name: "postgres",
+    labels: labels("postgres"),
+  },
+  spec: {
+    ports: [
+      {
+        port: 5432,
+        targetPort: 5432,
+      },
+    ],
+    selector: labels("postgres"),
+  },
+};
+
+// Ingress
+const tls = tlsEnabled
+  ? [{ hosts: [hostname], secretName: tlsSecretName }]
+  : [];
+
+const ingressAnnotations = tlsEnabled
+  ? {
+      "cert-manager.io/cluster-issuer": clusterIssuer,
+      "nginx.ingress.kubernetes.io/ssl-redirect": "true",
+    }
+  : {};
+
+const ingress = {
+  apiVersion: "networking.k8s.io/v1",
+  kind: "Ingress",
+  metadata: {
+    name: stack,
+    labels: labels(),
+    annotations: ingressAnnotations,
+  },
+  spec: {
+    tls,
+    rules: [
+      {
+        host: hostname,
+        http: {
+          paths: [
+            {
+              pathType: "Prefix",
+              path: "/",
+              backend: {
+                service: {
+                  name: nodeServiceName,
+                  port: {
+                    number: port,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+
+export function b64(str: string) {
+  return Buffer.from(str).toString("base64");
+}
+
+// only written if secretManaged is true
+const secret = {
+  apiVersion: "v1",
+  kind: "Secret",
+  type: "Opaque",
+  metadata: {
+    name: stack,
+    labels: labels(),
+  },
+  data: {
+    DATABASE_URL: b64(
+      `postgres://postgres:${insecurePostgresPassword}@postgres:5432/postgres`
+    ),
+    OIDC_CLIENT_SECRET: b64("kompassi_insecure_test_client_secret"),
+    OIDC_CLIENT_ID: b64("kompassi_insecure_test_client_id"),
+    SECRET_PASSWORD: b64("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
+  },
+};
+
+export function writeManifest(filename: string, manifest: unknown) {
+  writeFileSync(filename, JSON.stringify(manifest, null, 2), {
+    encoding: "utf-8",
+  });
+}
+
+function main() {
+  writeManifest("deployment.json", deployment);
+  writeManifest("service.json", service);
+  writeManifest("ingress.json", ingress);
+
+  if (postgresManaged) {
+    writeManifest("postgres-deployment.json", postgresDeployment);
+    writeManifest("postgres-service.json", postgresService);
+  } else {
+    if (existsSync("postgres-deployment.json")) {
+      unlinkSync("postgres-deployment.json");
+    }
+    if (existsSync("postgres-service.json")) {
+      unlinkSync("postgres-service.json");
+    }
+  }
+
+  if (secretManaged) {
+    writeManifest("secret.json", secret);
+  } else if (existsSync("secret.json")) {
+    unlinkSync("secret.json");
+  }
+}
+
+if (require.main === module) {
+  main();
+}
